90a0904027a72b9c4223bc2eef35902246d34628
Marwerk <marvin.saccarello@gmail.com>
2024-03-11 16:04:08 +0100

    Sync then authenticate

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
-const express = require('express');
-const { Sequelize, DataTypes } = require('sequelize');
-const dotenv = require('dotenv');
+const express = require("express")
+const { Sequelize, DataTypes } = require("sequelize")
+const dotenv = require("dotenv")
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres',
+    dialect: "postgres",
     logging: console.log, // Enable logging of SQL queries
-  });
+  }
+)
 
-const User = require('./models/User')(sequelize);
+const User = require("./models/User")(sequelize)
 
-app.post('/register', (req, res) => {
-    const { email, password, name } = req.body;
-    User.create({ email, password, name })
-      .then(() => {
-        res.send('User registered successfully');
-      })
-      .catch((error) => {
-        console.error('Error registering user:', error);
-        res.status(500).send('Error registering user');
-      });
-  });
+app.post("/register", (req, res) => {
+  const { email, password, name } = req.body
+  User.create({ email, password, name })
+    .then(() => {
+      res.send("User registered successfully")
+    })
+    .catch((error) => {
+      console.error("Error registering user:", error)
+      res.status(500).send("Error registering user")
+    })
+})
 
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Database connection has been established successfully.');
+    console.log("Database connection has been established successfully.")
     app.listen(3000, () => {
-      console.log('Server is running on port 3000');
-    });
+      console.log("Server is running on port 3000")
+    })
   })
   .catch((error) => {
-    console.error('Unable to connect to the database:', error);
-  });
+    console.error("Unable to connect to the database:", error)
+  })
