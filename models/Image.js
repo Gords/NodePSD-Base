159b7d7cd0c733f8049406589174a3ef2aeecb83
Marwerk <marvin.saccarello@gmail.com>
2024-03-12 16:13:20 +0100

    Fix Image model by generating directly with sequelize

--- models/Image.js
+++ models/Image.js
@@ -1,27 +1,26 @@
-// Model for the Image Table in the Database
-
-const { DataTypes } = require('sequelize')
-
-module.exports = (sequelize, User) => {
-  const Image = sequelize.define('Image', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: User,
-        key: 'id'
-      }
-    },
-    path: {
-      type: DataTypes.STRING,
-      allowNull: false
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Image.belongsTo(models.User, {
+        foreignKey: 'userId'
+      })
     }
-
-  })
-
-  return Image
-}
+  }
+  Image.init({
+    userId: DataTypes.STRING,
+    path: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Image',
+  });
+  return Image;
+};
