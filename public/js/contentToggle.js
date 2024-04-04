d5fecc699ffa6766042048d99ce383c229391273
Gords <gordsftrl@gmail.com>
2024-04-04 13:10:49 +0900

    changed refresh behaviour, it now just goes back to the top of the page (if it is not on in register or login)

--- public/js/contentToggle.js
+++ public/js/contentToggle.js
@@ -22,28 +22,25 @@ const route = () => {
   }
 };
 
-const linkClickHandler = (event) => {
-  event.preventDefault();
-  let path = event.target.getAttribute('href') || "/";
-  if (!path.startsWith("/")) path = "/" + path;
-  history.pushState({}, null, path);
-  route();
-};
 
-document.querySelectorAll("nav a").forEach(link => {
-  link.addEventListener("click", linkClickHandler);
-});
-
-window.addEventListener('popstate', () => {
-  route();
-});
-
+//Show/Hide content based in the id
 function showContent(contentId) {
   const contentSections = document.querySelectorAll('section[id$="-content"]');
   contentSections.forEach(section => {
     section.style.display = section.id === contentId ? 'block' : 'none';
   });
 }
+
+// Add event lister to widow object to go back to page begining on refresh (if not on login or register page)
+window.addEventListener('load', function () {
+  if (window.location.hash !== '' && window.location.hash !== '#login' && window.location.hash !== '#register') {
+    window.location.href = '/';
+  }
+});
+
+window.addEventListener('popstate', () => {
+  route();
+});
 
 // Call the route function initially
 route();
