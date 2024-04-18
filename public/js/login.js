415d98cb88ea2c37129ffc4623e4265867c5e8d1
Gords <gordsftrl@gmail.com>
2024-04-18 23:44:16 +0900

    fixed login success message

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
   const loggedIn = localStorage.getItem('loggedIn');
-
+  
   // Handle registration response
   document.body.addEventListener('htmx:afterSwap', function (event) {
-    const registerResponseContent = document.querySelector('#register-content #register-response');
-    if (registerResponseContent && registerResponseContent.contains(event.detail.elt)) {
-      const response = event.detail.xhr.response;
-
-      if (response.includes('alert-success')) {
-        // Registration successful, redirect after a delay
+    const registerFormComponent = document.getElementById('register-form-component');
+    if (registerFormComponent && registerFormComponent.contains(event.detail.target)) {
+      const alertSuccess = registerFormComponent.querySelector('.alert-success');
+      if (alertSuccess) {
         setTimeout(function() {
           window.location.href = '/';
         }, 2000);
-      } else if (response.includes('alert-error')) {
-        // Registration failed, display error message
-        registerResponseContent.innerHTML = response;
       }
     }
   });
-
 });
+
+
 
 // Logout function
 function logout() {
