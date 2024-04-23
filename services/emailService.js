3fea93d9acfe1ad506a09f4b606bce875caf2f17
Gords <gordsftrl@gmail.com>
2024-04-23 15:11:25 +0900

    testing ses

--- services/emailService.js
+++ services/emailService.js
@@ -1,27 +1,40 @@
-const { MailerSend, EmailParams, Sender, Recipient } = require("mailersend");
+const AWS = require("aws-sdk");
 
-const mailersend = new MailerSend({
-	apiKey: process.env.MAILERSEND_API_KEY,
+const ses = new AWS.SES({
+	region: process.env.AWS_REGION,
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 const sendVerificationEmail = async (email, token) => {
 	const verificationLink = `http://localhost:3000/verify-email?token=${token}`;
 
-	const sentFrom = new Sender("MS_rmBngK@pocket.sh", "flashcenter");
-	const recipients = [new Recipient(email)];
-
-	const emailParams = new EmailParams()
-		.setFrom(sentFrom)
-		.setTo(recipients)
-		.setSubject("Verifica tu cuenta de FlashCenter")
-		.setHtml(`<p>Por favor haz click aqui para verificar tu cuenta:</p>
-              <a href="${verificationLink}">${verificationLink}</a>`)
-		.setText(
-			`Por favor haz click aqui para verificar tu cuenta: ${verificationLink}`,
-		);
+	const params = {
+		Destination: {
+			ToAddresses: [email],
+		},
+		Message: {
+			Body: {
+				Html: {
+					Charset: "UTF-8",
+					Data: `<p>Please click the following link to verify your account:</p>
+						    <a href="${verificationLink}">${verificationLink}</a>`,
+				},
+				Text: {
+					Charset: "UTF-8",
+					Data: `Please click the following link to verify your account: ${verificationLink}`,
+				},
+			},
+			Subject: {
+				Charset: "UTF-8",
+				Data: "Verify your FlashCenter account",
+			},
+		},
+		Source: process.env.AWS_VERIFIED_EMAIL,
+	};
 
 	try {
-		await mailersend.email.send(emailParams);
+		await ses.sendEmail(params).promise();
 		console.log("Verification email sent successfully");
 	} catch (error) {
 		console.error("Error sending verification email:", error);
