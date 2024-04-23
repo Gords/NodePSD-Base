24e9bf241ce29714ac289625e5972867d5362c68
Gords <gordsftrl@gmail.com>
2024-04-24 00:06:47 +0900

    testing postmark

--- services/emailService.js
+++ services/emailService.js
@@ -1,47 +1,28 @@
-const AWS = require("aws-sdk");
+const postmark = require('postmark');
 
-const ses = new AWS.SES({
-	region: process.env.AWS_REGION,
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
 
 const sendVerificationEmail = async (email, token) => {
-	const verificationLink = `http://localhost:3000/verify-email?token=${token}`;
+  const verificationLink = `http://localhost:3000/verify-email?token=${token}`;
 
-	const params = {
-		Destination: {
-			ToAddresses: [email],
-		},
-		Message: {
-			Body: {
-				Html: {
-					Charset: "UTF-8",
-					Data: `<p>Please click the following link to verify your account:</p>
-						    <a href="${verificationLink}">${verificationLink}</a>`,
-				},
-				Text: {
-					Charset: "UTF-8",
-					Data: `Please click the following link to verify your account: ${verificationLink}`,
-				},
-			},
-			Subject: {
-				Charset: "UTF-8",
-				Data: "Verify your FlashCenter account",
-			},
-		},
-		Source: process.env.AWS_VERIFIED_EMAIL,
-	};
+  const mailOptions = {
+    From: process.env.POSTMARK_SENDER_EMAIL,
+    To: email,
+    Subject: 'Verify your FlashCenter account',
+    TextBody: `Please click the following link to verify your account: ${verificationLink}`,
+    HtmlBody: `<p>Please click the following link to verify your account:</p>
+               <a href="${verificationLink}">${verificationLink}</a>`,
+  };
 
-	try {
-		await ses.sendEmail(params).promise();
-		console.log("Verification email sent successfully");
-	} catch (error) {
-		console.error("Error sending verification email:", error);
-		throw new Error("Failed to send verification email");
-	}
+  try {
+    await client.sendEmail(mailOptions);
+    console.log('Verification email sent successfully');
+  } catch (error) {
+    console.error('Error sending verification email:', error);
+    throw new Error('Failed to send verification email');
+  }
 };
 
 module.exports = {
-	sendVerificationEmail,
+  sendVerificationEmail,
 };
