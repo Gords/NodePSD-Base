655b332b17b8ce88fbbd692278f72f13e45354e2
Marwerk <marvin.werkmeister@outlook.com>
2024-05-24 15:52:48 +0200

    Apply styling to contact form modal response

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -73,23 +73,23 @@ module.exports = () => {
 				});
 
 				res.send(`
-        <div id="contact-and-location-component">
-          <dialog class="modal modal-open" hx-ext="remove-me" remove-me="3s">
-            <div class="modal-box border-2 border-black text-center items-center">
-              <h3 class="font-bold text-lg">Mensaje enviado</h3>
-              <p class="py-4">Nuestros agentes se pondran en contacto.</p>
-            </div>
-          </dialog>
-        </div>
-      `);
+					<div id="contact-and-location-component">
+						<dialog class="modal modal-open" hx-ext="remove-me" remove-me="3s">
+							<div class="modal-box text-center items-center">
+								<h3 class="font-bold text-lg">Mensaje enviado</h3>
+								<p class="py-4">Nuestros agentes se pondran en contacto.</p>
+							</div>
+						</dialog>
+					</div>
+				`);
 			} catch (error) {
 				console.error("Error handling contact form submission:", error);
 				res.status(500).send(`
-        <div role="alert" class="alert alert-error max-w-sm mx-auto border-black">
-          <img src="./assets/icons/error.svg" alt="Error Symbol" class="w-6 h-6 inline-block">
-          <span class="font-bold text-center">Error submitting contact form. Please try again.</span>
-        </div>
-      `);
+					<div role="alert" class="alert alert-error max-w-sm mx-auto border-black">
+						<img src="./assets/icons/error.svg" alt="Error Symbol" class="w-6 h-6 inline-block">
+						<span class="font-bold text-center">Error submitting contact form. Please try again.</span>
+					</div>
+				`);
 			}
 		},
 	);
