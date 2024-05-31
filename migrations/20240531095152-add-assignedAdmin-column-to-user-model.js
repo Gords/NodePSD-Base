72711538b75d4208113849bb29e58d7d916058a8
Marwerk <marvin.werkmeister@outlook.com>
2024-05-31 12:03:23 +0200

    Update User model for assignedAdmin relationship + add corresponding migration

---
+++ migrations/20240531095152-add-assignedAdmin-column-to-user-model.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'assignedAdmin', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'assignedAdmin');
+  },
+};
